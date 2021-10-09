07f316e4b8ab1d07c699626f04dd3fae5c26a834
snaker938 <jmendel123@gmail.com>
2021-10-09 16:12:32 +0100

    Update Untitled-1.py

--- Random Testing Files/Untitled-1.py
+++ Random Testing Files/Untitled-1.py
@@ -1,46 +1,21 @@
-class create_form:
-    pupils = []
-    teachers = []
-    subjects = []
-    number_of_pupils = 0
-    number_of_teachers = 0
+pupils = []
+group1 = []
+group2 = []
 
-    def __init__(self, name):
-        self.name = name
+for i in range(0, 20):
+    pupils.append(i)
 
-
-    @classmethod
-    def add_pupil(cls, pupil_name):
-        cls.pupils.append(pupil_name)
-        cls.number_of_pupils += 1
-
-    @classmethod
-    def add_teacher(cls, teacher_name):
-        cls.teachers.append(teacher_name)
-        cls.number_of_teachers += 1
-
-    @classmethod
-    def add_subjects(cls, *args):
-        any(cls.subjects.append(x) for x in args)
+for i in range(0, 20, 2):
+    group1.append(pupils[i])
 
 
+for i in range(1, 20, 2):
+        group2.append(pupils[i])
 
 
-form1 = create_form("form1")
+print("GROUP 1")
+print(group1)
+print("")
+print("GROUP 2")
+print(group2)
 
-for i in range(0, 5):
-    form1.add_pupil(f"joshua{i}")
-
-form1.add_teacher("smith")
-form1.add_teacher("jonas")
-
-form1.add_subjects("maths", "chemistry", "biology", "physics")
-
-
-print(form1.name, form1.number_of_pupils, form1.number_of_teachers, form1.pupils, form1.teachers, form1.subjects)
-
-
-
-
-
-    
