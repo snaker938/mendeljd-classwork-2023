54e888937417d54a36bb422e97f8f25d45954ead
snaker938 <jmendel123@gmail.com>
2021-10-13 23:16:55 +0100

    added scoring

--- Computer Science Main/pygame/Pong/main.py
+++ Computer Science Main/pygame/Pong/main.py
@@ -3,6 +3,7 @@ import pygame
 from ball import Ball
 from paddle import Paddle
 from ball import Ball
+import time
  
 pygame.init()
  
@@ -47,22 +48,70 @@ playing = True
  
 # The clock will be used to control how fast the screen updates
 clock = pygame.time.Clock()
+
+
+#Initialise player scores
+scoreA = 0
+scoreB = 0
+
+
+
+
  
+
 # -------- Main Program Loop -----------
 while playing:
     # --- Main event loop
-    for event in pygame.event.get(): 
+    for event in pygame.event.get():
         if event.type == pygame.QUIT: 
               playing = False 
-        elif event.type==pygame.KEYDOWN:
-                if event.key==pygame.K_x: 
-                     playing=False
+        
 
 
     # --- Game logic should go here
 
-    
+    def reset():
+        screen.fill(BLACK)
+        pygame.draw.line(screen, WHITE, [349, 0], [349, 500], 5)
+        all_sprites_list.draw(screen) 
+        
+        #Display scores:
+        font = pygame.font.Font(None, 74)
+        text = font.render(str(scoreA), 1, WHITE)
+        screen.blit(text, (250,10))
+        text = font.render(str(scoreB), 1, WHITE)
+        screen.blit(text, (420,10))
 
+
+        pygame.display.flip()
+
+
+    
+    def create_timer(seconds):
+        start = time.time()
+        end = time.time()
+        counter = 1
+        while end - start < seconds:
+            while counter < 4:
+                # print("yep")
+                # font = pygame.font.Font(None, 74)
+                # text = font.render(str(counter), 1, WHITE)
+                # screen.blit(text, (150,70))
+                font = pygame.font.Font(None, 25)
+                text = font.render(str(counter), True, BLACK)
+                text_rect = text.get_rect(center=(700/2, 500/2))
+                screen.blit(text, text_rect)
+                time.sleep(0.5)
+                counter += 1
+            end = time.time()
+        return
+
+        
+
+
+
+    # while not counter_var:
+    
     all_sprites_list.update()
 
     keys = pygame.key.get_pressed()
@@ -74,16 +123,39 @@ while playing:
 
         
     if ball.rect.x>=690:
+        scoreA+=1
+        # point = True
+        ball.rect.x = 345
+        ball.rect.y = 195
         ball.velocity[0] = -ball.velocity[0]
+        reset()
+        create_timer(2)
+
+        # pygame.time.delay(3000)
     if ball.rect.x<=0:
+        scoreB+=1
+        # point = True
+        ball.rect.x = 345
+        ball.rect.y = 195
         ball.velocity[0] = -ball.velocity[0]
+        reset()
+        create_timer(2)
+
+        # pygame.time.delay(3000)
     if ball.rect.y>490:
         ball.velocity[1] = -ball.velocity[1]
     if ball.rect.y<0:
-        ball.velocity[1] = -ball.velocity[1] 
+        ball.velocity[1] = -ball.velocity[1]     
 
     if pygame.sprite.collide_mask(ball, paddleA) or pygame.sprite.collide_mask(ball, paddleB):
-      ball.bounce()
+        ball.bounce()
+
+    
+    if paddleB.rect.y < ball.rect.y:
+        paddleB.move_down(5)
+    elif paddleB.rect.y > ball.rect.y:
+        paddleB.move_up(5)
+    
  
  
     # --- Drawing code should go here
@@ -96,6 +168,17 @@ while playing:
     
     
     all_sprites_list.draw(screen) 
+
+
+    #Display scores:
+    font = pygame.font.Font(None, 74)
+    text = font.render(str(scoreA), 1, WHITE)
+    screen.blit(text, (250,10))
+    text = font.render(str(scoreB), 1, WHITE)
+    screen.blit(text, (420,10))
+
+    
+ 
  
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
